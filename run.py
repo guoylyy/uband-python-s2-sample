a2b993eedc109b8614eb5b3e47681aba8b57ed73
Liam Globit <guoylyy@gmail.com>
2017-08-08 14:11:32 +0800

    no message

--- run.py
+++ run.py
@@ -6,15 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    #read file
-    file = open('test.json', 'r+') #sdaf
-    file_text = file.read()
-    file_text = dict(json.loads(file_text))
-    return render_template('test.html',data=file_text)
+    return render_template('index.html')
 
-@app.route('/details/<string:student_number>')
-def details(student_number):
-    return render_template( str(student_number) + '.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
